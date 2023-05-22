a61165e5a7ee3e19479ef47909a826d33135609c
Arma Burton <arma@armaburton.com>
2023-05-22 16:11:45 -0700

    Write to file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 ﻿using System.Diagnostics;
 
+string logPath = Path.Combine(Environment.GetFolderPath(
+  Environment.SpecialFolder.DesktopDirectory), "log.txt");
+
+Console.WriteLine($"Writing to {logPath}");
+
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+
+Trace.Listeners.Add(logFile);
+
 Debug.WriteLine("Debug says, I am watching!");
 Trace.WriteLine($"Trace says I am watching!");
 
