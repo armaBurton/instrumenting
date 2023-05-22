36b790bd043750298f1788ef5d6e31158071bdb4
Arma Burton <arma@armaburton.com>
2023-05-22 16:47:03 -0700

    logging info

--- Program.cs
+++ Program.cs
@@ -1,41 +1,41 @@
-﻿using System.Diagnostics;
-using Microsoft.Extensions.Configuration;
+﻿// using System.Diagnostics;
+// using Microsoft.Extensions.Configuration;
 
-string logPath = Path.Combine(Environment.GetFolderPath(
-  Environment.SpecialFolder.DesktopDirectory), "log.txt");
+// string logPath = Path.Combine(Environment.GetFolderPath(
+//   Environment.SpecialFolder.DesktopDirectory), "log.txt");
 
-Console.WriteLine($"Writing to {logPath}");
+// Console.WriteLine($"Writing to {logPath}");
 
-TextWriterTraceListener logFile = new(File.CreateText(logPath));
+// TextWriterTraceListener logFile = new(File.CreateText(logPath));
 
-Trace.Listeners.Add(logFile);
+// Trace.Listeners.Add(logFile);
 
-Trace.AutoFlush = true;
+// Trace.AutoFlush = true;
 
-Debug.WriteLine("Debug says, I am watching!");
-Trace.WriteLine($"Trace says I am watching!");
+// Debug.WriteLine("Debug says, I am watching!");
+// Trace.WriteLine($"Trace says I am watching!");
 
-WriteLine($"Reading from appsettings.json in {0}",
-  arg0: Directory.GetCurrentDirectory());
+// WriteLine($"Reading from appsettings.json in {0}",
+//   arg0: Directory.GetCurrentDirectory());
 
-ConfigurationBuilder builder = new();
+// ConfigurationBuilder builder = new();
 
-builder.SetBasePath(Directory.GetCurrentDirectory());
+// builder.SetBasePath(Directory.GetCurrentDirectory());
 
-builder.AddJsonFile("appsettings.json",
-  optional: true, reloadOnChange: true);
+// builder.AddJsonFile("appsettings.json",
+//   optional: true, reloadOnChange: true);
 
-IConfigurationRoot configuration  = builder.Build();
+// IConfigurationRoot configuration  = builder.Build();
 
-TraceSwitch ts = new(
-  displayName: "PacktSwitch",
-  description: "This switch is set via a JSON config");
+// TraceSwitch ts = new(
+//   displayName: "PacktSwitch",
+//   description: "This switch is set via a JSON config");
 
-configuration.GetSection("PacktSwitch").Bind(ts);
+// configuration.GetSection("PacktSwitch").Bind(ts);
 
-Trace.WriteLineIf(ts.TraceError, $"Trace Error");
-Trace.WriteLineIf(ts.TraceWarning, $"Trace Warning");
-Trace.WriteLineIf(ts.TraceInfo, $"Trace Information");
-Trace.WriteLineIf(ts.TraceVerbose, $"Trace Verbose");
+// Trace.WriteLineIf(ts.TraceError, $"Trace Error");
+// Trace.WriteLineIf(ts.TraceWarning, $"Trace Warning");
+// Trace.WriteLineIf(ts.TraceInfo, $"Trace Information");
+// Trace.WriteLineIf(ts.TraceVerbose, $"Trace Verbose");
 
-ReadLine();
+// ReadLine();
