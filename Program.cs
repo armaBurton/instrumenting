654dcbaded1fcf8f57c82e3cadc5fae8d38fa61a
Arma Burton <arma@armaburton.com>
2023-05-22 16:06:26 -0700

    starting instrumenting over

--- Program.cs
+++ Program.cs
@@ -1,14 +1,5 @@
 ﻿using System.Diagnostics;
 
-string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt");
+Debug.WriteLine("Debug says, I am watching!");
+Trace.WriteLine($"Trace says I am watching!");
 
-Console.WriteLine($"Writing to: {logPath}");
-
-TextWriterTraceListener logFile = new(File.CreateText(logPath));
-
-Trace.Listeners.Add(logFile);
-
-Trace.AutoFlush = true;
-
-Debug.WriteLine($"Debug says, I am watching.");
-Trace.WriteLine($"Trace says, I am watching.");
