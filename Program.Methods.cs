5eab7279c1af174a446ec66b982952110dbef84a
Arma Burton <arma@armaburton.com>
2023-05-22 17:09:10 -0700

    dunno what's wrong

---
+++ Program.Methods.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+partial class Program{
+  static void LogSourceDetails(
+    bool condition,
+    [CallerMemberName] string member = "",
+    [CallerFilePath] string filePath = "",
+    [CallerLineNumber] int line = 0,
+    [CallerArgumentExpression(nameof(condition))] string expression = ""
+  ){
+    Trace.WriteLine(string.Format(
+      "[{0}]\n {1} on line {2}. Expression: {3}",
+      filePath, member, line, expression
+    ));
+  }
+}
